155477e032e06a331ef0802be0a7d9ebfa55f150
“Chenxing <chenxing.chi@estudiants.urv.cat>
2023-01-05 16:42:11 +0100

    afegir

---
+++ Practica3PROG/src/Exceptions/NoEsTrobaException.java
@@ -0,0 +1,14 @@
+package Exceptions;
+
+public class NoEsTrobaException extends Exception {
+
+		
+		public NoEsTrobaException(String text) {
+			super(text);
+		}
+
+		public NoEsTrobaException() {
+			
+		}
+		
+}
